5adc38f91ed9dee4d968cf2ed0f00c160d022738
Maria Mendonca <maria.mendonca156@gmail.com>
2023-09-22 10:37:00 +0530

    fixed styles

--- src/StyledComponents.js
+++ src/StyledComponents.js
@@ -7,17 +7,17 @@ export const QuizContainer = styled.div`
   background-color: #f5f5f5;
   border-radius: 8px;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
-  text-align: center;
 `;
 
 export const Question = styled.h2`
   font-size: 24px;
   margin-bottom: 20px;
   color: #333;
+  text-align: center;
 `;
 
-export const OptionsList = styled.ul`
-  list-style: none;
+export const OptionsList = styled.ol`
+  list-style-type: none;
   padding: 0;
 `;
 
@@ -26,23 +26,24 @@ export const OptionItem = styled.li`
   cursor: pointer;
   padding: 10px;
   border-radius: 4px;
+  border: 1px solid #eee7ed;
   transition: background-color 0.3s ease-in-out;
 
   &:hover {
-    background-color: #007bff;
-    color: #fff;
+    border: 1px solid #ac118a;
   }
 
   ${(props) =>
     props.selected &&
     `
-    background-color: #28a745;
-    color: #fff;
+    background-color: #f5def2;
+    border: 1px solid #ac118a;
+    color: #630454;
   `}
 `;
 
 export const NextButton = styled.button`
-  background-color: #007bff;
+  background-image: linear-gradient(to right, #9d176c, #a77da4);
   color: #fff;
   border: none;
   padding: 10px 20px;
@@ -52,24 +53,31 @@ export const NextButton = styled.button`
   margin-top: 20px;
   transition: background-color 0.3s ease-in-out;
 
-  &:hover {
-    background-color: #0056b3;
-  }
-
   &:disabled {
-    background-color: #0011b3;
+    background-image: linear-gradient(to right, #eee7ed, #eee7ed);
+    color: #ffeeee;
   }
 `;
 
 export const QuizCompletedMessage = styled.p`
-  font-size: 18px;
-  margin-top: 20px;
-  color: #333;
+  font-size: 40px;
+  text-align: center;
+  margin: 20px 0;
+  color: #9d176c;
+  font-weight: bold;
+`;
+export const QuizMainHeading = styled.p`
+  font-size: 40px;
+  text-align: center;
+  margin: 20px 0;
+  color: #000;
+  font-weight: bold;
+  padding-top: 2rem;
 `;
 
 export const Score = styled.p`
   font-size: 24px;
   font-weight: bold;
   margin-top: 20px;
-  color: #28a745; /* Green color for correct score */
+  color: #691249;
 `;
