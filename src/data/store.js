e5a32cb65a460ca5bc71c137d851ff4a4384b446
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-12 08:32:01 +0530

    Changed name, added new category in store

--- src/data/store.js
+++ src/data/store.js
@@ -1,7 +1,7 @@
 import { create } from 'zustand';
 
 const useStore = create((set) => ({
-  questions: [
+  computerQuestions: [
     {
       id: 1,
       question: 'Using Microsoft word we can simply ___',
@@ -93,6 +93,100 @@ const useStore = create((set) => ({
       correctAnswer: 'Cache Memory',
     },
   ],
+
+  animalQuestions: [
+    {
+      id: 1,
+      question: 'Which animal is known as the king of the jungle?',
+      options: [
+        'Lion',
+        'Tiger',
+        'Elephant',
+        'Monkey',
+      ],
+      correctAnswer: 'Lion',
+    },
+    {
+      id: 2,
+      question: 'What do birds use to fly?',
+      options: ['Legs', 'Wings', 'Tail', 'Ears'],
+      correctAnswer: 'Wings',
+    },
+    {
+      id: 3,
+      question: 'Which animal has the longest neck?',
+      options: ['Camel', 'Elephant', 'Zebra', 'Giraffe'],
+      correctAnswer: 'Giraffe',
+    },
+    {
+      id: 4,
+      question: 'The two kinds of main memory are',
+      options: [
+        'CDs and DVDs',
+        'RAM and ROM',
+        'Primary and secondary',
+        'Direct and sequential',
+      ],
+      correctAnswer: 'RAM and ROM',
+    },
+    {
+      id: 5,
+      question: 'RAM is also called as',
+      options: [
+        'Virtual memory',
+        'Volatile memory',
+        'Non volatile memory',
+        'Cache memory',
+      ],
+      correctAnswer: 'Volatile memory',
+    },
+    {
+      id: 6,
+      question: 'Internal hard disk is',
+      options: ['Removable but not fixed', 'Removable', 'Not fixed', 'Fixed'],
+      correctAnswer: 'Fixed',
+    },
+    {
+      id: 7,
+      question: 'The main memory of computer is also called as',
+      options: [
+        'Hard-disk',
+        'Internal memory',
+        'Primary storage',
+        'Secondary storage',
+      ],
+      correctAnswer: 'Primary storage',
+    },
+    {
+      id: 8,
+      question: 'How much data can be stored in a CD?',
+      options: ['300 MB', '400 MB', '670 MB', '700 MB'],
+      correctAnswer: '700 MB',
+    },
+    {
+      id: 9,
+      question: 'CD-ROM stands for',
+      options: [
+        'Compactable Disc Read Only Memory',
+        'Compact Data Read Only Memory',
+        'Compact Disc Read Only Memory',
+        'Compactable Read Only Memory',
+      ],
+      correctAnswer: 'Compact Disc Read Only Memory',
+    },
+    {
+      id: 10,
+      question: '_____ has the shortest access times.',
+      options: [
+        'Virtual Memory',
+        'Secondary Memory',
+        'Cache Memory',
+        'All of the above',
+      ],
+      correctAnswer: 'Cache Memory',
+    },
+  ],
+
   currentQuestionIndex: 0,
   setCurrentQuestionIndex: (currentQuestionIndex) =>
     set(() => ({ currentQuestionIndex })),
@@ -118,9 +212,12 @@ const useStore = create((set) => ({
 
   result: false,
   setResult: (result) => set(() => ({ result })),
-
+  
   correctAnswers: 0,
   setCorrectAnswers: (result) => set(() => ({ result })),
+
+  // selectedCategory: null,
+  // setSelectedCategory: (category) => set(() => ({ category })),
 }));
 
 export default useStore;
