20977bf7c2ad5a182bb20b9d85d57392b5b4be51
Maria Mendonca <maria.mendonca156@gmail.com>
2024-08-15 09:14:30 +0530

    Added Text Constants

---
+++ src/data/textConstants.js
@@ -0,0 +1,24 @@
+const TEXT_CONSTANTS = {
+  //* Headings
+  HOME_HEADER: 'Select a Category to proceed',
+  PREVIEW_HEADER: 'PREVIEW RESULTS BEFORE SUBMITTING',
+  RESULT_HEADER: 'RESULT',
+
+  //* Result Text
+  TOTAL_QUESTIONS: 'Total Questions: ',
+  CORRECT_ANSWERS: 'Correct Answers: ',
+  WRONG_ANSWERS: 'Wrong Answers: ',
+  REMARKS: 'Remarks: ',
+
+  //* Remarks
+  EXCELLENT: 'Excellent',
+  VERY_GOOD: 'Very Good',
+  GOOD: 'Good',
+  BETTER_LUCK: 'Better Luck Next Time',
+
+  //* Button Texts
+  NEXT: 'NEXT',
+  BACK: 'BACK',
+};
+
+export default TEXT_CONSTANTS;
